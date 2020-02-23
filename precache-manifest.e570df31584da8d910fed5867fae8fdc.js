c10d2cc969dd466cc0a5a2c3a36b489d51096c63
rriamarria <fmariamarinescu@gmail.com>
2020-02-23 11:24:51 +0200

    Updates

---
+++ precache-manifest.e570df31584da8d910fed5867fae8fdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c55bea0004c995a85508a4bdcc8df27",
+    "url": "/learning_how_to_code/index.html"
+  },
+  {
+    "revision": "83119fd0f38ee0972956",
+    "url": "/learning_how_to_code/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c8e19f352f7c15132415",
+    "url": "/learning_how_to_code/static/css/main.245f8696.chunk.css"
+  },
+  {
+    "revision": "83119fd0f38ee0972956",
+    "url": "/learning_how_to_code/static/js/2.4360f707.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/learning_how_to_code/static/js/2.4360f707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e19f352f7c15132415",
+    "url": "/learning_how_to_code/static/js/main.3c8eccc6.chunk.js"
+  },
+  {
+    "revision": "2be2e1b685bd40616d71",
+    "url": "/learning_how_to_code/static/js/runtime-main.3bee76ce.js"
+  }
+]);
