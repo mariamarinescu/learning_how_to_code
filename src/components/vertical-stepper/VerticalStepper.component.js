7616ce93912f0b3093324a61760735230493bb71
rriamarria <fmariamarinescu@gmail.com>
2020-02-28 10:44:25 +0200

    edits resWCS (pre-last step of stepper) course-syllabus

--- src/components/vertical-stepper/VerticalStepper.component.js
+++ src/components/vertical-stepper/VerticalStepper.component.js
@@ -109,6 +109,7 @@ function resCredis() {
         <ul>
             <li>Program : <a href="https://www.wildcodeschool.com/en-GB/trainings/web-developer-full-time?campus=bucharest" target="_blank" rel="noopener norefferer"> JS Fullstack Developer Bootcamp</a></li>
             <li>Course syllabus: <a href="https://www.wildcodeschool.com/en-GB/trainings/web-developer-full-time?campus=bucharest&fbclid=IwAR1KjYFSgZ19fB-vj1wsNuiu_FKE17ytp3_7ZuEaYj3gYsG6Ku89mmW0Eok" target="_blank" rel="noopener norefferer">Click Me</a></li>
+            <li>From day one we started to work as teams, using Agile Methodologies, using <a href="https://www.trello.com" target="_blank" rel="noopener norefferer">Trello</a> for task for Scrum sprints and sprints backlog.</li>
             <li>3 team projects:</li>
         </ul>
         <VerticalLinearStepper />
